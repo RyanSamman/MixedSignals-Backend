50e68fc7476749ec14a16b2d33e7423e2c25d164
RyanSamman <RyanSamman@outlook.com>
2020-05-29 02:41:13 +0300

    Linting & Export fix

--- src/models/Stocks.js
+++ src/models/Stocks.js
@@ -4,36 +4,36 @@ const { Schema } = mongoose;
 
 // Stocks/Crypto Schemata
 const schemaQuote = new Schema({
-  day: {
-    type: Date,
-    index: true,
-  },
-  price: { type: mongoose.Types.Decimal128 },
-  volume: { type: mongoose.Types.Decimal128 },
+	day: {
+		type: Date,
+		index: true,
+	},
+	price: { type: mongoose.Types.Decimal128 },
+	volume: { type: mongoose.Types.Decimal128 },
 });
 
 const schemaStock = new Schema({
-  name: {
-    type: String,
-    required: [true, 'You must give a name to the stock'],
-    trim: true,
-    uppercase: true,
-    unique: true,
-  },
+	name: {
+		type: String,
+		required: [true, 'You must give a name to the stock'],
+		trim: true,
+		uppercase: true,
+		unique: true,
+	},
 
-  // make into enum?
-  type: {
-    type: String,
-    enum: ['Crypto', 'Stock'],
-  },
-  prices: [schemaQuote],
+	// make into enum?
+	type: {
+		type: String,
+		enum: ['Crypto', 'Stock'],
+	},
+	prices: [schemaQuote],
 },
 // Adds createdAt and updatedAt fields into the record
 {
-  timestamps: true,
+	timestamps: true,
 });
 
 // Compile into model
 const Stock = mongoose.model('Stock', schemaStock);
 
-exports.Stock = Stock;
+module.exports = Stock;
--- src/models/Users.js
+++ src/models/Users.js
@@ -7,25 +7,25 @@ const { Schema } = mongoose;
 // Nested in User_S to be able to change more easily
 // TODO: Refine NewsPreferences schema
 const schemaNewsPreference = new Schema({
-  type: String,
-  subtype: [String],
+	type: String,
+	subtype: [String],
 });
 
 const schemaUser = new Schema({
-  name: {
-    type: String,
-    required: [true, 'Testing'],
-    index: true,
-  },
-  // TODO: Find best cryptographic hash for passwords (MD5)
-  password: String,
-  // TODO: Find a way to autogen salt
-  // Appending the salt to the password before hashing ensures that it can't be
-  // looked up on a rainbow table, so even if the database was breached, hackers cannot
-  // decrypt the password
-  // https://en.wikipedia.org/wiki/Rainbow_table
-  salt: String,
-  preferences: [schemaNewsPreference],
+	name: {
+		type: String,
+		required: [true, 'Testing'],
+		index: true,
+	},
+	// TODO: Find best cryptographic hash for passwords (MD5)
+	password: String,
+	// TODO: Find a way to autogen salt
+	// Appending the salt to the password before hashing ensures that it can't be
+	// looked up on a rainbow table, so even if the database was breached, hackers cannot
+	// decrypt the password
+	// https://en.wikipedia.org/wiki/Rainbow_table
+	salt: String,
+	preferences: [schemaNewsPreference],
 });
 
 // Compile Schema --> Mongoose Model
@@ -37,4 +37,4 @@ const schemaUser = new Schema({
 const User = mongoose.model('User', schemaUser /* , collectionName */);
 
 // Export Model
-exports.User = User;
+module.exports = User;
