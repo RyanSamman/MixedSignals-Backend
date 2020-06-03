bf6b294af95a73f1944ee5bab31304c59c921f2b
RyanSamman <RyanSamman@outlook.com>
2020-06-03 20:22:42 +0300

    Refactoring Database Loading

--- src/index.js
+++ src/index.js
@@ -1,27 +1,32 @@
 // Give strings color, to highlight errors etc https://www.npmjs.com/package/chalk
 const chalk = require('chalk');
 
-// Config file
-const config = require('./config');
-
-console.log('Config Files Loaded!:\n', config);
-
 // Throws error if any occur uncaught inside a promise
 process.on('unhandledRejection', (err) => {
 	console.error(chalk.bgRed.black('⚠ Unhandled Rejection was not caught:\n', err));
 	throw err;
 });
 
-// Load Database
-const dbconnection = require('./loaders/db');
+async function main() {
+	// Load Config file
+	const config = require('./config');
+	console.log(chalk.bgBlue.black('\n Config Files Loaded!: \n'), config, '\n');
 
-console.log(dbconnection);
+	// Load Database
+	const databaseLoader = require('./loaders/loadDatabase');
+	let { timeToConnect } = await databaseLoader(config);
+	console.log(chalk.bgBlue.black(` Time to connect to Database: ${timeToConnect} Seconds \n`));
 
-// Load Express routes
-const app = require('./loaders/express');
+	// Load Express routes
+	const app = require('./loaders/express');
 
-// 404 - Not Found route; Must be last route
-app.use(require('./services/404'));
+	// 404 - Not Found -  route; Must be last route
+	app.use(require('./services/404'));
 
-// Bind config port to Express
-app.listen(config.PORT, () => console.log(`Server started on port ${config.PORT}`));
+	// Bind config port to Express
+	app.listen(config.PORT, () => {
+		console.log(chalk.bgBlue.black(` Server started on port ${config.PORT} \n`));
+	});
+}
+
+main();
---
+++ src/loaders/loadDatabase.js
@@ -0,0 +1,40 @@
+const chalk = require('chalk');
+const mongoose = require('mongoose');
+
+async function loadDatabase({MONGO_URI, DB_NAME}) {
+	let initialTime = new Date();
+	try {
+		await mongoose.connect(MONGO_URI, {
+			useNewUrlParser: true,
+			useUnifiedTopology: true,
+			useCreateIndex: true,
+			dbName: DB_NAME,
+		});
+	} catch (err) {
+		// Log any errors which may occur during connection
+		console.log(err);
+		throw err;
+	}
+	const connection = mongoose.connection;
+
+	// Listening for errors after connection has been established
+	connection.on('error', (err) => {
+		console.log(chalk.bgRed.black(`connection error:, ${err.reason}`));
+	});
+
+	mongoose.connection.on('disconnected', () => {
+		console.log(chalk.bgRed.black('Mongoose default connection is disconnected'));
+	});
+
+	mongoose.connection.on('SIGINT', () => {mongoose.connection.close(() => {
+		console.log('Connection Closed, disconnecting database');
+		process.exit(0);
+	});});
+
+	// Do a task once connection has opened
+	//connection.once('open', () => {})
+	let timeToConnect = (new Date() - initialTime)/1000;
+	return {timeToConnect, connection};
+}
+
+module.exports = loadDatabase;
